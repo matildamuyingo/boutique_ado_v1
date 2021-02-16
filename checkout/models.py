9b391ff80a6055cbe77003b5bfbb796ed4f1ebea
matildamuyingo <matilda.muyingo@hotmail.com>
2021-02-16 11:54:27 +0000

    Added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -16,7 +16,7 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=240, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
@@ -29,7 +29,9 @@ class Order(models.Model):
         return uuid.uuid4.hex.upper()
 
     def update_total(self):
-        """ Update grand total each time a line item is added, accounting for delivery costs. """
+        """ 
+        Update grand total each time a line item is added, accounting for delivery costs. 
+        """
 
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
@@ -52,7 +54,7 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
